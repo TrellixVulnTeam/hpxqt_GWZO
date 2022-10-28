0bf4ec52c0b4fb682e3296f3ccb1151e77379f8a
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-28 03:59:53 +0000

    Adding tarfile member sanitization to extractall()

--- upgrade.py
+++ upgrade.py
@@ -88,7 +88,26 @@ class WindowUpdateMixIn(object):
     def process_linux(self):
         with tarfile.open(self.download_file) as tar:
             # specify path explicitly to extract files to download_dir
-            tar.extractall(path=os.path.join(self.download_dir.name))
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, path=os.path.join(self.download_dir.name))
             # Get path to executable
             src_dir = os.path.join(self.download_dir.name, tar.getnames()[-1])
             self._rename_executable()
